5042ab07c75409d435c91d20e9c54e385f31fc7a
GregHanCompany <greghan@olymposnetworks.com>
2024-11-10 15:24:22 +0900

    Refactor GoogleAuthGuard, JwtAuthGuard, and RefreshAuthGuard
    Remove unused imports and update the guard classes to remove unnecessary code.

--- src/auth/guards/jwt-auth/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth/jwt-auth.guard.ts
@@ -1,7 +1,6 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from 'src/auth/decorators/public.decorator';
 
 @Injectable()
